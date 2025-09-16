b934901b12c0c2d5637082597a7550cd4cfb4289
Mohammad-ikram-sarkar <islamikram357@gmail.com>
2025-09-16 14:44:50 +0600

    iphone details ui 2

--- src/app/components/macbookcomponents/Macbookdetails.tsx
+++ src/app/components/macbookcomponents/Macbookdetails.tsx
@@ -522,7 +522,7 @@ Please let me know more details!`;
                     </svg>
                     Region
                   </h3>
-                  <div className="grid grid-cols-3 gap-2">
+                  <div className="grid grid-cols-2 max-sm:grid-cols-3 gap-2">
                     {product.dynamicRegions.map((region) => (
                       <OptionButton
                         key={region.name}
