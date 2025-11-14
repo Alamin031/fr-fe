69298c5ce10beb7eedaab2665bdce83b0a4c491e
Mohammad-ikram-sarkar <islamikram357@gmail.com>
2025-11-14 18:22:27 +0600

    benar fix

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -160,7 +160,7 @@ const HeroBannerSlider: React.FC = () => {
 
   return (
     <section 
-        className="w-full max-w-7xl mx-auto p-4" 
+        className="w-full  mx-auto p-4" 
         aria-label="Promotional Banner"
     >
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-4">
